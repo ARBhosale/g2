9994157a61269569682427d0cf376f814d75d5ac
System Administrator <admin@example.com>
2024-08-21 22:01:40 -0700

    ccc

--- src/server/script.js
+++ src/server/script.js
@@ -1,6 +1,7 @@
 import { gs } from '@servicenow/glide'
 const a = 47;
 const b = 48;
+const c = 49;
 export function showStateUpdate(current, previous) {
     const currentState = current.getValue('state')
     const previousState = previous.getValue('state')
